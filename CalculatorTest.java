e2d5bc5f28e4aaa72bf52b362cd92a41ec456566
Gerardo Solis <56048406+Sol-Jerry@users.noreply.github.com>
2022-03-21 10:48:19 -0700

    Updated CalculatorTest class

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -2,12 +2,20 @@ package Chapter_6.atHome;
 
 public class CalculatorTest 
 {
+    
+    /** 
+     * @param args
+     */
     public static void main(String[] args)
     {
         String expression = "a*b/(c-a)+d*e";
         if(BalanceChecker.checkBalance(expression) == true)
         {        
             String postfix = ConvertToPostfix.convertToPostfix(expression); // Expression with no parenthesis
+           
+            System.out.println("After evaluating the expression " + expression 
+            + " we get the following postfix expression: " + postfix); // print obtained value
+
             int postfixValue = EvaluatePostfix.evaluatePostfix(postfix); // Get value from postfix expression
             
             System.out.println("After evaluating the expression " + expression 
